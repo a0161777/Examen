f3ee3b2331077e804680b5a28de1fcc023b40e72
a0161777 <a01613777@tec.mx>
2025-12-04 08:52:10 -0600

    Update costos (1).py

--- costos (1).py
+++ costos (1).py
@@ -12,58 +12,30 @@ st.header("Descripción de la predicción")
 
 def user_input_features():
 
-    City = st.number_input(
-        'Ciudad (1- Acapulco, 2- Acuña o 3- Aguascalientes):',
-        min_value=1,
-        max_value=3,
-        value=1,
-        step=1
-    )
+    City = st.number_input('Ciudad (1- Acapulco, 2- Acuña o 3- Aguascalientes):', min_value=1, max_value=3,value=1,step=1)
 
 
-    Month = st.number_input(
-        'Mes (de 1 a 12 según sea el mes):',
-        min_value=1,
-        max_value=12,
-        value=1,
-        step=1
-    )
+    Month = st.number_input('Mes (de 1 a 12 según sea el mes):', min_value=1,max_value=12, value=1,step=1)
 
 
-    Year = st.number_input(
-        'Año (desde 1800 a 2013):',
-        min_value=1800,
-        max_value=2013,
-        value=2000,
-        step=1
-    )
+    Year = st.number_input( 'Año (desde 1800 a 2013):',min_value=1800, max_value=2013, value=2000, step=1)
 
    
-    user_input_data = {
-        "Cuidad ( 1- Acapulco, 2- Acuña y 3 - Aguascalientes)": City,
-        "Mes(del 1 al 12 según sea el mes)": Month,
-        "Año (desde 1800 a 2013)": Year
-    }
-
-  
+    user_input_data = {"Cuidad ( 1- Acapulco, 2- Acuña y 3 - Aguascalientes)": City, "Mes(del 1 al 12 según sea el mes)": Month, "Año (desde 1800 a 2013)": Year}
     features = pd.DataFrame(user_input_data, index=[0])
     return features
 
 
 
 df = user_input_features()
-
-
+prediccion= 0
 datos = pd.read_csv("Temperature.csv", encoding="latin-1")
-
-
 X = datos.drop(columns="AverageTemperature")
 y = datos["AverageTemperature"]
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1613777)
-
 LR = LinearRegression()
 LR.fit(X_train, y_train)
 
