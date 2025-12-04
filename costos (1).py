818f0578b6897869524dd95869afedb0010794db
a0161777 <a01613777@tec.mx>
2025-12-04 08:20:37 -0600

    Update costos (1).py

--- costos (1).py
+++ costos (1).py
@@ -1,7 +1,7 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 
 # Título de la app
